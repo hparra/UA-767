6b108bfc33feff9320d06c07d7f204fb3bff8777
hparra <hector@hectorparra.com>
2010-03-17 00:21:24 -0700

    Initial commit of old BloodPressureMonitor index and new JS file.

---
+++ UA-767.js
@@ -0,0 +1,77 @@
+/* depends on jQuery */
+// TODO: check for jQuery or inject it
+
+// A&D UA-767(PC|BT) Digital Blood Pressure Monitor
+UA767 = {
+	
+	// Checks to see is string is valid UA-767 return data e.g. '804C4C4960'
+	// returns string if valid or null if not
+	validateData = function(data) {
+		// check length = 10
+		// check codes = {80, 81, 82, 83}
+	},
+	
+	// 80--------
+	// returns string
+	parseCode = function(data) {
+		
+	},
+	
+	// --4C------
+	// returns integer
+	parsePulsePressure = function(data) {
+		
+	},
+	
+	// ----4C----
+	// returns integer
+	parseDiastolicPressure = function(data) {
+		
+	},
+	
+	// ------49--
+	// returns integer
+	parsePulsePerMinute = function(data) {
+		
+	},
+	
+	// I have no idea what this value represents
+	// --------60
+	// returns integer
+	parseUnknown = function(data) {
+		
+	},
+	
+	// PulsePressure + DiastolicPressure
+	// returns integer
+	parseSystolicPressure = function(data) {
+		
+	}
+}
+
+updateUI = function() {
+	
+	data = "804C4C4960";
+	
+	code = parseCode(data);
+	
+	if (code == "80") {
+		Ps = UA767.parseSystolicPressure(data);
+		Pd = UA767.parseDiastolicPressure(data);
+		bpm = UA767.parsePulsePerMinute(data);
+		
+		$("#sys").text(Ps);
+		$("#dia").text(Pd);
+		$("#pul").text(bpm);
+	}
+	else {
+		$("#sys").text("");
+		$("#dia").text("");
+		$("#pul").text("Err");
+	}
+};
+
+
+$(document).ready(function() {
+	updateUI();
+});
